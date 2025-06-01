e12a8ce40bcab20c106037c7d551f701278ba921
kDick91 <kdickson91@gmail.com>
2025-06-01 15:30:49 -0500

    added album listing and buttons

--- lib/Settings/AlbumNotificationsSettings.php
+++ lib/Settings/AlbumNotificationsSettings.php
@@ -5,26 +5,67 @@ namespace OCA\AlbumNotifications\Settings;
 use OCP\Settings\ISettings;
 use OCP\IUserSession;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IConfig;
+use OCP\IURLGenerator;
+use OCP\Http\Client\IClientService;
 
 class AlbumNotificationsSettings implements ISettings {
-  private $userSession;
+    private $userSession;
+    private $config;
+    private $urlGenerator;
+    private $clientService;
+    private $userId;
 
-  public function __construct(IUserSession $userSession) {
-    $this->userSession = $userSession;
-  }
+    public function __construct(IUserSession $userSession, IConfig $config, IURLGenerator $urlGenerator, IClientService $clientService) {
+        $this->userSession = $userSession;
+        $this->config = $config;
+        $this->urlGenerator = $urlGenerator;
+        $this->clientService = $clientService;
+        $this->userId = $userSession->getUser()->getUID();
+    }
 
-  public function getForm() {
-    $user = $this->userSession->getUser();
-    $displayName = $user ? $user->getDisplayName() : 'Unknown';
-    $parameters = ['user' => $displayName];
-    return new TemplateResponse('album_notifications', 'settings', $parameters);
-  }
+    public function getForm() {
+        $displayName = $this->userSession->getUser()->getDisplayName();
 
-  public function getSection() {
-    return 'album_notifications';
-  }
+        // Fetch albums from Photos app
+        $client = $this->clientService->newClient();
+        $myAlbumsUrl = $this->urlGenerator->linkToRouteAbsolute('photos.albums.myAlbums');
+        $sharedAlbumsUrl = $this->urlGenerator->linkToRouteAbsolute('photos.albums.sharedAlbums');
 
-  public function getPriority() {
-    return 10;
-  }
+        try {
+            $myAlbumsResponse = $client->get($myAlbumsUrl);
+            $myAlbums = json_decode($myAlbumsResponse->getBody(), true) ?? [];
+        } catch (\Exception $e) {
+            $myAlbums = [];
+        }
+
+        try {
+            $sharedAlbumsResponse = $client->get($sharedAlbumsUrl);
+            $sharedAlbums = json_decode($sharedAlbumsResponse->getBody(), true) ?? [];
+        } catch (\Exception $e) {
+            $sharedAlbums = [];
+        }
+
+        // Combine albums
+        $albums = array_merge($myAlbums, $sharedAlbums);
+
+        // Get selected albums from config
+        $selectedAlbumsJson = $this->config->getUserValue($this->userId, 'album_notifications', 'selected_albums', '[]');
+        $selected_albums = json_decode($selectedAlbumsJson, true);
+
+        $parameters = [
+            'user' => $displayName,
+            'albums' => $albums,
+            'selected_albums' => $selected_albums,
+        ];
+        return new TemplateResponse('album_notifications', 'settings', $parameters);
+    }
+
+    public function getSection() {
+        return 'album_notifications';
+    }
+
+    public function getPriority() {
+        return 10;
+    }
 }
--- templates/settings.php
+++ templates/settings.php
@@ -1,3 +1,40 @@
 <?php
+use OCP\Util;
 ?>
 <p>Hello <?php p($user); ?></p>
+<table>
+    <thead>
+        <tr>
+            <th>Album Name</th>
+            <th>Daily Notification</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($albums as $album): ?>
+            <tr>
+                <td><?php p($album['name']); ?></td>
+                <td>
+                    <input type="checkbox" class="album-checkbox" value="<?php p($album['id']); ?>"
+                        <?php if (in_array($album['id'], $selected_albums)) echo 'checked'; ?> />
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<input type="hidden" name="selected_albums" id="selected_albums" value="<?php p(json_encode($selected_albums)); ?>" />
+<script>
+    $(document).ready(function() {
+        const $checkboxes = $('.album-checkbox');
+        const $selectedInput = $('#selected_albums');
+        
+        function updateSelected() {
+            const selected = $checkboxes.filter(':checked').map(function() { return this.value; }).get();
+            $selectedInput.val(JSON.stringify(selected));
+        }
+        
+        $checkboxes.on('change', updateSelected);
+        
+        // Initial update
+        updateSelected();
+    });
+</script>
